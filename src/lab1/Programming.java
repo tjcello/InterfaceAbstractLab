6f035adc534ae0c0531393df4aff3426a9aeeb70
Thomas <Thomas@10.30.18.213>
2015-09-15 11:11:39 -0500

    added programming class

---
+++ src/lab1/Programming.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1;
+
+/**
+ *
+ * @author Thomas
+ */
+public abstract class Programming {
+    private String courseName;
+    private String courseNumber;
+    private double credits;
+}
